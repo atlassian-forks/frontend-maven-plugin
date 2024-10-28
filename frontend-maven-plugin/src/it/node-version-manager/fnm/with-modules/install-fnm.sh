b44a977624274d5811a700603c7de7a81a72c4e4
fnowakowski <fnowakowski@atlassian.com>
2024-10-28 18:27:01 +0100

    DCA11Y-1145: remove test that can't be set

--- frontend-maven-plugin/src/it/node-version-manager/fnm/with-modules/install-fnm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# invoker env variables are not loaded yet
-export HOME="$(dirname "$0")"
-
-export FNM_DIR="$HOME/.fnm";
-mkdir "$FNM_DIR"
-
-curl -fsSL https://fnm.vercel.app/install | bash -s -- --install-dir "$FNM_DIR" --skip-shell --force-install
