30afd62d749d0fb41cb6abc31ac4adc49a85e7d0
fnowakowski <fnowakowski@atlassian.com>
2024-11-22 12:33:28 +0100

    fix fnm install

--- frontend-maven-plugin/src/it/node-version-manager/fnm/with-webpack/install-fnm.sh
+++ frontend-maven-plugin/src/it/node-version-manager/fnm/with-webpack/install-fnm.sh
@@ -8,5 +8,5 @@ mkdir "$FNM_DIR"
 
 curl -fsSL https://fnm.vercel.app/install | bash -s -- --install-dir "$FNM_DIR" --skip-shell --force-install
 
-eval "$(fnm env)" \
+eval "$($HOME/.fnm/fnm env)" \
         && fnm install 20.15.1
