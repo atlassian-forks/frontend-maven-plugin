0abce14c816faa3f7b069a45571cc47b0fc996e3
fnowakowski <fnowakowski@atlassian.com>
2024-12-06 10:35:51 +0100

    fix custom node assertion

--- frontend-maven-plugin/src/it/node-version-manager/asdf/verify.groovy
+++ frontend-maven-plugin/src/it/node-version-manager/asdf/verify.groovy
@@ -1,7 +1,7 @@
 import org.codehaus.plexus.util.FileUtils
 
 // assert
-assert !new File(basedir, 'node').exists() : "Node was installed in the custom install directory"
+assert !new File(basedir, 'node/node').exists() : "Node was installed in the custom install directory"
 assert new File(basedir, 'node_modules').exists() : "Node modules were not installed in the base directory"
 
 String buildLog = FileUtils.fileRead(new File(basedir, 'build.log'))
