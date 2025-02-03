25759ab8a1e81e77fdad12dfdcaa926a4059d0e7
fnowakowski <fnowakowski@atlassian.com>
2025-02-03 12:19:17 +0100

    add windows fnm support

--- frontend-maven-plugin/src/it/node-version-manager/windows/fnm/install-fnm.ps1
+++ frontend-maven-plugin/src/it/node-version-manager/windows/fnm/install-fnm.ps1
@@ -1,8 +1,10 @@
 # Set the HOME directory to the directory of the script
-$HOME = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$WORKING_DIR = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+Write-Output $WORKING_DIR
 
 # Set the FNM_DIR variable
-$FNM_DIR = Join-Path -Path $HOME -ChildPath ".fnm"
+$FNM_DIR = Join-Path -Path $WORKING_DIR -ChildPath ".fnm"
 
 # Create the directory if it does not exist
 if (-Not (Test-Path -Path $FNM_DIR)) {
@@ -11,12 +13,13 @@ if (-Not (Test-Path -Path $FNM_DIR)) {
 
 # Download and execute the FNM install script
 $zipUrl = "https://github.com/Schniz/fnm/releases/download/v1.38.1/fnm-windows.zip"
-$zipFilePath = "$FNM_DIR\installer"
+$zipFilePath = "$FNM_DIR\installer.zip"
 Invoke-WebRequest -Uri $zipUrl -OutFile $zipFilePath
 
 $destinationPath = $FNM_DIR
 Expand-Archive -Path $zipFilePath -DestinationPath $destinationPath -Force
 
 # Load the FNM environment and install Node.js version 20.15.1
-"$FNM_DIR\fnm.exe" env --use-on-cd --shell powershell | Out-String | Invoke-Expression
-"$FNM_DIR\fnm.exe" install 20.15.1
+Invoke-Expression "$FNM_DIR\fnm.exe install 20.15.1 --fnm-dir $FNM_DIR"
+
+Write-Output "Finished FNM installation"
