f4506ac1d96d66462a79df76176dde8d0d208965
Michael Kemp <mkemp@atlassian.com>
2025-08-13 18:58:21 +0200

    DCA11Y-2101: Add corepack IT

---
+++ frontend-maven-plugin/src/it/node-version-manager/fnm/with-corepack/install-fnm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# invoker env variables are not loaded yet
+export HOME="$(dirname "$0")"
+
+export FNM_DIR="$HOME/.fnm";
+mkdir "$FNM_DIR"
+
+curl -fsSL https://fnm.vercel.app/install | bash -s -- --install-dir "$FNM_DIR" --skip-shell --force-install
+
+eval "$($HOME/.fnm/fnm env --corepack-enabled)" \
+        && $HOME/.fnm/fnm install 23.1.0
